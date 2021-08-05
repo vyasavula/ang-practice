8f03b6d7271ee142b1e3b216043b4737556c264f
your username <vyasavula6@gmail.com>
2021-08-05 14:42:34 +0530

    Intial COmmit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddRestoComponent } from './add-resto/add-resto.component';
+import { ListRestoComponent } from './list-resto/list-resto.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UpdateRestoComponent } from './update-resto/update-resto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: AddRestoComponent,
+    path: 'add',
+  },
+  {
+    component: ListRestoComponent,
+    path: 'list',
+  },
+  {
+    component: LoginComponent,
+    path: 'login',
+  },
+  {
+    component: RegisterComponent,
+    path: 'register',
+  },
+  {
+    component: UpdateRestoComponent,
+    path: 'update',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { AddRestoComponent } from './add-resto/add-resto.component';
+import { ListRestoComponent } from './list-resto/list-resto.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AddRestoComponent, ListRestoComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
